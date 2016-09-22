867f200ec99ae8e6fe9a3599f14bb7646228c93e
Colter Hooker <ceh248@nau.edu>
2016-09-22 00:38:20 -0700

    new start to the project. just can read in the data to memory

--- main.c
+++ main.c
@@ -1,29 +1,101 @@
-//
-// Created by Colter Hooker on 9/13/16.
-//
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
-#include "ppmrw.h"
+typedef struct {
+    unsigned char r, g, b;
+}RGB_pixal;
 
-int main(int argc, char *argv[]){
-    // Take in four aruments from the user
-    printf("Enter programming name, number, input and output file");
-    if (argv < 4){
-        perror("Too few arguments");
-        exit(1);
+RGB_pixal *buffer;
+
+int width;
+int height;
+
+typedef struct {
+    int x,y;
+    struct PPM_image *data;
+}PPM_image;
+
+void skip_ws(FILE* ppm){
+    int c = fgetc(ppm);
+    while (isspace(c)){
+        c = fgetc(ppm);
     }
-    if ( argv > 4){
-        perror("Too many arguments");
-        exit(0);
-    }
-    int format = atoi(avgv[1]);
-    const char *input_file = argv[2];
-    const char *output_file = argv[3];
-
-    //Image *img;
-
-    printf("Program done running");
+    ungetc(c,ppm);
 }
 
+void ppmrw(const char* ppm){
+    FILE* fp= fopen(ppm,"rb");
+    char buffer[1024];
+    malloc(sizeof(buffer));
+    int c = fgetc(fp);
+    int i = 0;
+    buffer[i] = 0;
+    while (c != EOF){
+        buffer[i] = c;
+        c = fgetc(fp);
+        i+=1;
+    }
+    skip_ws(fp);
+    /*while (c != ' '){
+        buffer[i] = c;
+        c = fgetc(fp);
+        i+=1;
+    }
+    skip_ws(fp);
+    while (c != '#'){
+        buffer[i] = c;
+        c = fgetc(fp);
+        i+=1;
+    }
+    skip_ws(fp);*/
+
+    if (buffer[0] !='P'){
+        fprintf(stderr,"Error not correct FILE");
+        exit(1);
+    }
+    if (buffer[1] !='3' || buffer[1] != '6'){
+        fprintf(stderr,"Error not correct FILE");
+        exit(1);
+    }
+    return;
+}
+
+// ok write to the file now
+void write_P3(const char* p3){
+    FILE* fp = fopen(p3,"wb");
+    //fwrite();
+}
+
+void write_P6(){
+
+}
+
+int main (int argc, char *argv[]){
+    if (argc != 4){
+        fprintf(stderr,"Error not correct amount of argc: \n");
+        exit(1);
+    }
+
+    // each of the argv to a value
+    int format = atoi(argv[1]);
+/*    char *input_file = argv[2];
+    char *output_file = (argv[3]);*/
+
+    // run ppmrw
+    const char *input_file = argv[2];
+    ppmrw(input_file);
+
+    if (format == '3'){
+        const char *output_file = argv[3];
+        write_P3(output_file);
+    }
+    if (format == '6'){
+        write_P6();
+    }
+
+    // ok do thing before we get here
+
+}
