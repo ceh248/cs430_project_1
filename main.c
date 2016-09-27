6474c5778f29beb591bb2ffa2572d2249840d6d5
Colter Hooker <ceh248@nau.edu>
2016-09-26 19:53:28 -0700

    p3 write in teh correct format

--- main.c
+++ main.c
@@ -18,59 +18,58 @@ typedef struct {
     struct PPM_image *data;
 }PPM_image;
 
-void skip_ws(FILE* ppm){
-    int c = fgetc(ppm);
-    while (isspace(c)){
-        c = fgetc(ppm);
-    }
-    ungetc(c,ppm);
-}
 
-void ppmrw(const char* ppm){
-    FILE* fp= fopen(ppm,"rb");
-    char buffer[1024];
-    malloc(sizeof(buffer));
+void ppmrw(const char* ppmin,const char* ppmout, int format){
+    FILE* fp= fopen(ppmin,"rb");
+
+    fseek(fp,0L,SEEK_END);
+    rewind(fp);
+
+    char *std = malloc(sizeof(fp));
+    //char buffer[] = malloc(sizeof(fp));
     int c = fgetc(fp);
     int i = 0;
-    buffer[i] = 0;
+    std[i] = 0;
     while (c != EOF){
-        buffer[i] = c;
-        c = fgetc(fp);
-        i+=1;
+        if(c == '\n'){
+            c = fgetc(fp);
+        }else if(isspace(c)){
+            c = fgetc(fp);
+        } else if(c == '#'){
+            // need to skip a line
+            c = fgetc(fp);
+        } else{
+            std[i] = c;
+            c = fgetc(fp);
+            i+=1;
+        }
     }
-    skip_ws(fp);
-    /*while (c != ' '){
-        buffer[i] = c;
-        c = fgetc(fp);
-        i+=1;
-    }
-    skip_ws(fp);
-    while (c != '#'){
-        buffer[i] = c;
-        c = fgetc(fp);
-        i+=1;
-    }
-    skip_ws(fp);*/
 
-    if (buffer[0] !='P'){
-        fprintf(stderr,"Error not correct FILE");
-        exit(1);
+    if (format == 3){
+        //char* take_along = std;
+        write_P3(ppmout,std);
     }
-    if (buffer[1] !='3' || buffer[1] != '6'){
-        fprintf(stderr,"Error not correct FILE");
-        exit(1);
+    if (format == 6){
+        write_P6(ppmout,std);
     }
     return;
 }
 
-// ok write to the file now
-void write_P3(const char* p3){
+// ok write to the file in p6 format right now.
+void write_P3(const char* p3, char* take_along){
     FILE* fp = fopen(p3,"wb");
-    //fwrite();
+    //char *std = malloc(sizeof(take_along));
+    //fwrite(take_along,sizeof(take_along),sizeof(take_along),fp);
+
+    //fclose(fp);
+    return;
 }
-
-void write_P6(){
-
+// write to the file in p6 format
+void write_P6(const char* p6, char*take_along){
+    FILE* fp = fopen(p6,"wb");
+    //char *std = malloc(sizeof(take_along));
+    fwrite(take_along,sizeof(take_along),sizeof(take_along),fp);
+    return;
 }
 
 int main (int argc, char *argv[]){
@@ -81,21 +80,10 @@ int main (int argc, char *argv[]){
 
     // each of the argv to a value
     int format = atoi(argv[1]);
-/*    char *input_file = argv[2];
-    char *output_file = (argv[3]);*/
 
     // run ppmrw
     const char *input_file = argv[2];
-    ppmrw(input_file);
-
-    if (format == '3'){
-        const char *output_file = argv[3];
-        write_P3(output_file);
-    }
-    if (format == '6'){
-        write_P6();
-    }
-
-    // ok do thing before we get here
+    const char *output_file = argv[3];
+    ppmrw(input_file,output_file,format);
 
 }
